1f7d9340b192af115974011a6e93d76084ad4153
ali <alimotahari565@gmail.com>
2023-09-25 19:08:09 +0330

    commit

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,56 +3,12 @@ import LastNote from "./lastNote/LastNote";
 import Poshe from "./Poshe/Poshe";
 import More from "./More/More";
 import { Search } from "react-bootstrap-icons";
-import { Plus } from "react-bootstrap-icons";
 import { useState } from "react";
 
 function NavBar() {
-  const [isShowAddNote, setisShowAddNote] = useState(false);
-  const [newNoteValueTitle, setNewNoteValueTitle] = useState();
-  const [newNoteValueDes, setNewNoteValueDes] = useState();
-  const [newNoteValueDate, setNewNoteValueDate] = useState();
-  const [selectedColor, setSelectedColor] = useState();
-  const [nextId2, setNextId2] = useState(3);
-  const [Notes, setNotes] = useState(cards);
+ 
 
-
-  const cancelHandlerBtnAddNote = () => {
-    setisShowAddNote(false);
-  };
-
-  const handlchangBtnAddNoteTitle = (event) => {
-    setNewNoteValueTitle(event.target.value);
-  };
-  const handlchangBtnAddNoteDes = (event) => {
-    setNewNoteValueDes(event.target.value);
-  };
-  const handlchangBtnAddNoteDate = (event) => {
-    setNewNoteValueDate(event.target.value);
-  };
-
-  const handleColorSelect = (coloor) => {
-    setSelectedColor(coloor);
-  };
-
-  const handlClickAddNote = () => {
-    setisShowAddNote(true);
-  };
-
-
-  const handlChangOKnewNote = () => {
-    const newNote = {
-      title: newNoteValueTitle,
-      description: newNoteValueDes,
-      date: newNoteValueDate,
-      color: selectedColor,
-      id: nextId2,
-    };
-    const upNoteNew = [...Notes, newNote];
-    setNotes(upNoteNew);
-    setisShowAddNote(false);
-    setNextId2(nextId2 + 1)
-    // localStorage.setItem("notes", JSON.stringify(upNoteNew));
-  };
+ 
   return (
     <div className="nav">
       <div className="parent">
@@ -62,73 +18,8 @@ function NavBar() {
             <Search className="serc" />
           </h4>
         </div>
-        <div className="button">
-          <button className="button1" onClick={handlClickAddNote}>
-            <Plus className="plus" />
-            یادداشت جدید
-          </button>
-        </div>
-        {isShowAddNote && (
-          <form>
-            <div className="add-note-paren">
-              <input
-                type="text"
-                className="add_note"
-                placeholder="موضوع خود را وارد کنید"
-                onChange={handlchangBtnAddNoteTitle}
-              />
-              <input
-                type="text"
-                className="add_note"
-                placeholder="متن خود را وارد کنین"
-                onChange={handlchangBtnAddNoteDes}
-              />
-              <input
-                type="date"
-                className="add_note"
-                placeholder="تارییخ را وارد کنین"
-                onChange={handlchangBtnAddNoteDate}
-              />
-            </div>
-            <div className="selectColor">
-              <div
-                className="color1"
-                onClick={() => handleColorSelect("#38A3A5")}
-              ></div>
-              <div
-                className="color2"
-                onClick={() => handleColorSelect("#8377D1")}
-              ></div>
-              <div
-                className="color3"
-                onClick={() => handleColorSelect("#4C86A8")}
-              ></div>
-              <div
-                className="color4"
-                onClick={() => handleColorSelect("#ac2121")}
-              ></div>
-              <div
-                className="color5"
-                onClick={() => handleColorSelect("#bdbf43")}
-              ></div>
-            </div>
-
-            <div className="add_note_ok_cancel">
-              <button
-                className="btn-ok-new-folder"
-                onClick={handlChangOKnewNote}
-              >
-                ثبت
-              </button>
-              <button
-                className="btn-cancel-new-folder"
-                onClick={cancelHandlerBtnAddNote}
-              >
-                برگشت
-              </button>
-            </div>
-          </form>
-        )}
+        
+       
         <LastNote />
 
         <Poshe />
--- src/component/NavBar/Poshe/Poshe.jsx
+++ src/component/NavBar/Poshe/Poshe.jsx
@@ -1,117 +1,136 @@
-import './Poshe.css'
+import "./Poshe.css";
 import folderIcon from "@/assets/images/icons/folder.svg";
 import NewFolderIcon from "@/assets/images/icons/new-folder.svg";
 import OpenFolderIcon from "@/assets/images/icons/open-folder.svg";
-import { Plus } from 'react-bootstrap-icons';
-import { XLg } from 'react-bootstrap-icons';
+import { Plus } from "react-bootstrap-icons";
 import Btn from "@/component/Ui/Btn/Btn";
 
-
 import NavBarItem from "../NavBarItem/NavBarItem";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const initialPoshes = [
-    {
-        id: 1,
-        name: "همه یاد‌داشت‌‌ها",
-      },
-      {
-        id: 2,
-        name: "کاری",
-      },
-      {
-        id: 3,
-        name: "مسافرت",
-      },
-      {
-        id: 4,
-        name: "رویدادها",
-      },
-      {
-        id: 5,
-        name: "باشگاه",
-      },
+  {
+    id: 1,
+    name: "همه یاد‌داشت‌‌ها",
+  },
+  {
+    id: 2,
+    name: "کاری",
+  },
+  {
+    id: 3,
+    name: "مسافرت",
+  },
+  {
+    id: 4,
+    name: "رویدادها",
+  },
+  {
+    id: 5,
+    name: "باشگاه",
+  },
 ];
 
 function Poshe() {
-  const [selectPoshe , setSelectPoshe] = useState(null);
-  const [isShowFolder , setisShowFolder] = useState(false);
+  const [selectPoshe, setSelectPoshe] = useState(null);
+  const [isShowFolder, setisShowFolder] = useState(false);
   const [NewFolderValue, setNewFolderValue] = useState();
   const [Folders, setFolders] = useState(initialPoshes);
   const [nextId, setNextId] = useState(6);
 
-
   const handlerBtnAddFolder = () => {
-    setisShowFolder(true)
-  }
+    setisShowFolder(true);
+  };
 
   const cancelHandlerBtnAddFolder = () => {
-    setisShowFolder(false)
-  }
+    setisShowFolder(false);
+  };
 
+  const handlchang = (event) => {
+    setNewFolderValue(event.target.value);
+  };
 
-    const handlchang = (event) => {
-      setNewFolderValue(event.target.value)
-    }
+  const handlchangOK = () => {
+    const newFolder = { name: NewFolderValue, id: nextId };
+    const updatedFolders = [...Folders, newFolder];
+    setFolders(updatedFolders);
+    setisShowFolder(false);
+    setNewFolderValue("");
+    setNextId(nextId + 1);
+    console.log("salam");
 
-     useEffect(() => {
+    localStorage.setItem("folders", JSON.stringify(updatedFolders));
+  };
+  useEffect(() => {
     const storedFolders = localStorage.getItem("folders");
     if (storedFolders) {
       setFolders(JSON.parse(storedFolders));
     }
   }, []);
 
-
-    const handlchangOK = () => {
-      const newFolder = { name: NewFolderValue, id: nextId };
-      const updatedFolders = [...Folders, newFolder];
-      setFolders(updatedFolders);
-      setisShowFolder(false);
-      setNewFolderValue('');
-      setNextId(nextId + 1);
-    
-      // ذخیره پوشه‌ها در لوکال استوریج
-      localStorage.setItem("folders", JSON.stringify(updatedFolders));
+  useEffect(() => {
+    const handlFolder = (event) => {
+      const isClickInForm = event.target.closest(".folder, header");
+      if (!isClickInForm) {
+        setisShowFolder(false);
+      }
     };
-    
+    if(isShowFolder){
+      document.body.addEventListener("click", handlFolder);
+    }
+    return () => {
+      document.body.removeEventListener("click", handlFolder);
+    }
+  }, [isShowFolder]);
 
-    return (
-        <section className='title-poshe'>
-            <header>
-                <h2>پوشه ها</h2>
-                <button className='new-folder-button'>
-                    <img className='NewFolderIcon' src={NewFolderIcon} onClick={handlerBtnAddFolder} />
-                </button>
-            </header>
 
-            {isShowFolder &&(
-              <div >
-              <form className='folder' onSubmit={handlchangOK}>
-                <img src={NewFolderIcon}/>
-                <input type="text" className='input-new-folder' onChange={handlchang} placeholder='نام پوشه ...'/>
-                {/* <button className='btn-ok-new-folder' onClick={handlchangOK}><Plus /></button>
-                <button className='btn-cancel-new-folder' ><XLg /></button> */}
-                <Btn type='submit' variant="ok">ثبت<Plus /></Btn>
-                <Btn onClick={cancelHandlerBtnAddFolder} variant="cancel">cancel</Btn>
-              </form>
-            </div>
-            )}
+  return (
+    <section className="title-poshe">
+      <header>
+        <h2>پوشه ها</h2>
+        <button className="new-folder-button">
+          <img
+            className="NewFolderIcon"
+            src={NewFolderIcon}
+            onClick={handlerBtnAddFolder}
+          />
+        </button>
+      </header>
 
-            <div className='allfolder'>
-                {Folders.map((item, index) => (
-                    <NavBarItem 
-                    key={item.id} 
-                    text={item.name} 
-                    selectPoshe={item.id === selectPoshe}
-                    event={()=> setSelectPoshe(item.id)}
-                    icon={index === 0 ? OpenFolderIcon : folderIcon}
-                    />
-                ))}
+      {isShowFolder && (
+        <div>
+          <form className="folder" onSubmit={handlchangOK}>
+            <img src={NewFolderIcon} />
+            <input
+              type="text"
+              className="input-new-folder"
+              onChange={handlchang}
+              placeholder="نام پوشه ..."
+            />
+            <Btn type="submit" variant="ok">
+              ثبت
+              <Plus />
+            </Btn>
+            <Btn onClick={cancelHandlerBtnAddFolder} variant="cancel">
+              cancel
+            </Btn>
+          </form>
+        </div>
+      )}
 
-            </div>
-            
-        </section>
-    );
+      <div className="allfolder">
+        {Folders.map((item, index) => (
+          <NavBarItem
+            key={item.id}
+            text={item.name}
+            selectPoshe={item.id === selectPoshe}
+            event={() => setSelectPoshe(item.id)}
+            icon={index === 0 ? OpenFolderIcon : folderIcon}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Poshe;
